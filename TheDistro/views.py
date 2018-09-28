e24b708ad72d17639da4b36c02e6b3a42d33afb5
TestAccount2077 <maselectronics594@gmail.com>
2018-09-28 16:12:43 +0200

    Commit

--- TheDistro/views.py
+++ TheDistro/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def post_data(request):
     
     data = request.POST
